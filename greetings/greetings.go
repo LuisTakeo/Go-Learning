c1683f11c98eb4ee619a71a40ab5fbf7f0d724cf
LuisTakeo <luistakeo@hotmail.com>
2024-01-16 03:31:21 -0300

    Criado funções Hello, Hellos, Test, tratativa de erro, etc

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,54 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	// message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat())
+	return message, nil
+}
+
+// func Hello(name string) (string, error) {
+// 	// If no name was given, return an error with a message.
+// 	if name == "" {
+// 		return name, errors.New("empty name")
+// 	}
+// 	// Create a message using a random format.
+// 	message := fmt.Sprintf(randomFormat(), name)
+// 	return message, nil
+// }
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Takeo", "Chicão", "Charlinha"}
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println(messages[names[1]])
+	// fmt.Println(messages)
+	for _, message := range messages {
+		fmt.Println(message)
+	}
 }
+
+// Run the following command to install the module:
+// go mod init example.com/greetings -> create a go.mod file
+// go mod tidy -> add the module's dependencies to the go.mod file
+// go run . -> run main.go
+// go build -> build the module into an executable file
+// go install -> install the module as a binary in the workspace's bin directory
+
+// Para rodar o programa:
+// go mod init example.com/greetings -> cria um arquivo go.mod
+// go mod tidy -> adiciona as dependencias do modulo no arquivo go.mod
+// go run -> roda o arquivo main
+// go build -> cria um executavel
+// go install -> cria um executavel e instala no workspace/bin
